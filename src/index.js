f8bc92acd0cc8117ace00c7497d7531e09e92e3c
estouaprendendo <estouaprendendo@gmail.com>
2019-04-06 16:46:52 -0300

    Video aula de graphql - fazt - https://www.youtube.com/watch?v=Wl8O6wW4FJU&feature=em-uploademail

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import graphqlHTTP from "express-graphql";
 import schema from "./schema";
 
+import { connect } from "./database";
+
 const app = express();
+
+connect();
 
 app.get('/', (req, res) => {
     res.json({
@@ -12,7 +16,10 @@ app.get('/', (req, res) => {
 
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
-    schema: schema
+    schema: schema,
+    context: {
+        messageId: 'test'
+    }
 }));
 
 app.listen(3000, () => console.log('Server on port 3000'));
--- src/schema.js
+++ src/schema.js
@@ -5,6 +5,42 @@ const typeDefs = `
     
     type Query {
         hello: String
+        greet(name: String!): String
+        tasks: [Task]
+        Users: [User]
+    }
+    
+    type Task {
+        _id: ID
+        title: String!
+        description: String!
+        number: Int
+    }
+    
+    type User {
+        _id: ID
+        firstName: String!
+        lastName: String!
+        age: Int
+    }
+    
+    type Mutation {
+        createTask(input: TaskInput): Task
+        createUser(input: UserInput): User
+        deleteUser(_id: ID): User
+        updateUser(_id: ID, input: UserInput): User
+    }
+    
+    input TaskInput {
+        title: String!
+        description: String!
+        number: Int
+    }
+    
+    input UserInput {
+        firstName: String!
+        lastName: String!
+        age: Int
     }
 `;
 
